e6bcde8ce0f2c7dd93da01262cec6c07e4385600
Roberto <roberto.quadraccia90@gmail.com>
2024-02-14 11:35:26 +0100

    single meetup fetching dynamically

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,13 @@
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "@/components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://greatruns.com/wp-content/uploads/2018/04/Kata-Beach-Michael-T.jpg"
-      title="First Meetup"
-      address="Some address 5, 12345 Some City"
-      description="This is a first meetup!"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
@@ -16,6 +17,20 @@ const MeetupDetails = () => {
 // getStaticProps executes for every page so for every meetup ID value allows you to fetch data
 // for that meetup and allows you to return props for that meetup.
 export const getStaticPaths = async () => {
+  const client = await MongoClient.connect(
+    "mongodb+srv://robertoquadraccia90:KJ6YX6atbI8065Cz@cluster0.z8a93pe.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  // The first argument {} is used to filter, in our case we wanna find all so we leave it empty
+  // The second argument is to define which fields should be returned, by default are all.
+  // We use _id: 1 to fetch all the object but just the id
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     // If you set fallback to FALSE, you say that your paths contains all supported meetup ID values.
     // It means that if the user enters anything that's not supported here, for example M3,
@@ -26,32 +41,38 @@ export const getStaticPaths = async () => {
     // for specific meetup ID values.
 
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 };
 
 export const getStaticProps = async (context) => {
   const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://robertoquadraccia90:KJ6YX6atbI8065Cz@cluster0.z8a93pe.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  // The first argument {} is used to filter, in our case we wanna find all so we leave it empty
+  // The second argument is to define which fields should be returned, by default are all.
+  // We use _id: 1 to fetch all the object but just the id
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId), // ObjectId allows us to compare meetupId with _id
+  });
+
+  client.close();
   return {
     props: {
       meetupData: {
-        image:
-          "https://greatruns.com/wp-content/uploads/2018/04/Kata-Beach-Michael-T.jpg",
-        id: meetupId,
-        title: "First Meetup",
-        address: "Some address 5, 12345 Some City",
-        description: "This is a first meetup!",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
